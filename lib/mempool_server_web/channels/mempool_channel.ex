b0190170f9ed08c0b2522dbbde80874996341138
c8e4 <c8e4@proton.me>
2024-12-31 21:21:53 +0100

    channel wip

--- lib/mempool_server_web/channels/mempool_channel.ex
+++ lib/mempool_server_web/channels/mempool_channel.ex
@@ -3,22 +3,40 @@ defmodule MempoolServerWeb.MempoolChannel do
 
   alias MempoolServer.TransactionsCache
 
+  # When the client requests to join "mempool:transactions"
   def join("mempool:transactions", _message, socket) do
-    # On channel join, immediately send the current mempool transactions
+    # 1) Queue up an internal message to ourselves (the channel process)
+    send(self(), :after_join_all)
+
+    # 2) Respond with {:ok, socket} so Phoenix knows the channel is joined
+    {:ok, socket}
+  end
+
+  # When the client requests to join "mempool:sigmausd_transactions"
+  def join("mempool:sigmausd_transactions", _message, socket) do
+    # 1) Queue up an internal message
+    send(self(), :after_join_sigmausd)
+
+    # 2) Respond with {:ok, socket}
+    {:ok, socket}
+  end
+
+  # Our "after_join_all" handler is called after the socket is joined.
+  # Now we can safely push the data we want to send.
+  def handle_info(:after_join_all, socket) do
     all_transactions = TransactionsCache.get_all_transactions()
     push(socket, "all_transactions", %{transactions: all_transactions})
-
-    {:ok, socket}
+    {:noreply, socket}
   end
 
-  def join("mempool:sigmausd_transactions", _message, socket) do
-    # On channel join, immediately send the SigmaUSD-filtered transactions
+  # Similarly for sigmausd
+  def handle_info(:after_join_sigmausd, socket) do
     sigmausd_transactions = TransactionsCache.get_sigmausd_transactions()
     push(socket, "sigmausd_transactions", %{transactions: sigmausd_transactions})
-
-    {:ok, socket}
+    {:noreply, socket}
   end
 
+  # Standard no-op for any inbound events
   def handle_in(_event, _params, socket) do
     {:noreply, socket}
   end
